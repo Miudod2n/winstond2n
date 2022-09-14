569d8ef7d99b3b1023432659f48d9be78b6dccce
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2022-09-14 20:10:08 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
 client.on('ready', ()=>{
     console.log('I am ready!');
     setTimeout(function(){ // in leftToAttack() milliseconds run this:
