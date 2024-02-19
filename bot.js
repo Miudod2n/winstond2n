ddac18de720c696fe3062f4c504928538dc76def
Miudod2n <29958119+Miudod2n@users.noreply.github.com>
2024-02-19 16:52:27 -0300

    Update bot.js
    removing old twinoid links from dictionary

--- bot.js
+++ bot.js
@@ -94,21 +94,21 @@ client.on('message', message =>{
 
         switch (resto){
             case "6FO":case "6fo": 
-                resultado = "6FO is an acronym to **6 Feet Over**, a die2nite meta. 6FO was created on Season 7 by Dream and lasted until Season 13 when it dissolved to form new metas. Their notable members list can be found here: https://twinoid.com/g/6-feet-over/members";break;
+                resultado = "6FO is an acronym to **6 Feet Over**, a die2nite meta. 6FO was created on Season 7 by Dream and lasted until Season 13 when it dissolved to form new metas.";break;
             case "7sins":case "7Sins": 
-                resultado = "7sins is an acronym to **S7v7n Zedly Sins, a die2nite meta. 7sins was created around Season 3 and is currently being managed by Sashimi. Notable players: https://twinoid.com/g/7-zedly-sins-die2nite-meta# chat https://discord.gg/8JuJpqK";break;  
+                resultado = "7sins is an acronym to **S7v7n Zedly Sins, a die2nite meta. 7sins was created around Season 3 and is currently being managed by Sashimi. Discord server https://discord.gg/8JuJpqK";break;  
             case "9ap diamond": case "9AP diamond": case "diamond":
                 resultado = "**9 AP Diamond** is an organized town strategy where players seek to go out and scavenge at 9AP away from town. The result of this is a shape of a diamond in the map. This is done for a number of reasons. Since food and water are the only boosters available on day 1, one player can go up to 9AP away from town counting 6AP base + 6/7AP food + water, round trip. It is always better to deplete zones further from town first as the zones closer to town will get depleted by players returning from their scavenge zones. The diamond also gives some intel on the disposition of nearby ruins the town has before Watchtower kicks in. On organized towns it is customary to select a 9AP diamond spot before the town even start. Some cornered towns might not have a complete diamond as they can be 6AP away from the edge of the map. Here is a day 1 example of a 9AP diamond. https://github.com/Miudod2n/winstond2n/raw/master/images/9apd.png";break;
             case "AS": case "as":
                 resultado = "AS is an acronym that stands for **A**uto **S**earch. It describes the act of waiting in a zone for the next auto-search. It have variations like \"ASing\" (Auto-searching)";break;
             case "BD":case "bd": 
-                resultado = "BD is an acronym to **Benevolent Dictators**, a die2nite meta. BD was created on Season 10 by ValiumCereal and lasted until Season 12. More details: https://twinoid.com/g/benevolent-dictators-meta#";break;   
+                resultado = "BD is an acronym to **Benevolent Dictators**, a die2nite meta. BD was created on Season 10 by ValiumCereal and lasted until Season 12.";break;   
             case "BT":case"bt":
                 resultado = "BT is an acronym to **Breakthrough**, a die2nite ability. BT was awarded to all players on the end of Season 4 as a consolation for the 3 days the game went down (Epoqalypse) killing several towns. This ability consist of a 50% chance of killing one zombie in the zone and taking control back for a few minutes. It can only be used once per town. Citizens with the Armageddon Witness distinction have this skill.";break;
             case "COS":case "cos": 
-                resultado = "COS is an acronym to **Coalition of Stares**, a die2nite meta. COS was created around Season 5. More information: https://twinoid.com/g/cos#";break;
+                resultado = "COS is an acronym to **Coalition of Stares**, a die2nite meta. COS was created around Season 5.";break;
             case "DoA":case "doa":case "DOA": 
-                resultado = "DoA is an acronym to **Drunkards of the Apocalypse**, a die2nite meta. DoA was created around Season 10 by Ivana. More information: https://twinoid.com/g/d2n-meta-doa#";break;
+                resultado = "DoA is an acronym to **Drunkards of the Apocalypse**, a die2nite meta. DoA was created around Season 10 by Ivana.";break;
             case "EC":case "ec":
                 resultado = "EC stands for **E**lectric **C**omponent, a rare resource used on several constructions and items. http://d2nwiki.com/wiki/Electronic_component";break;
             case "EE": case "ee":
@@ -118,11 +118,11 @@ client.on('message', message =>{
             case "EWB": case "ewb":
                 resultado = "EWB is an acronym for **E**xploding **W**ater **B**omb, a water weapon that can be made out of Water Ration + Plastic Bag and Semtex (plastic bag + duct tape + compact detonator + semtex). http://d2nwiki.com/wiki/Exploding_Water_Bomb";break;
             case "hippies": case "Hippies":
-                resultado = "hippies is an acronym to **Happy Hippies**, a die2nite meta that did social jumps (whatever that means). hippies was created by Joanne and Babooshka76 around Season 6. More information: https://twinoid.com/g/happy-hippies#";break;   
+                resultado = "hippies is an acronym to **Happy Hippies**, a die2nite meta that did social jumps (whatever that means). hippies was created by Joanne and Babooshka76 around Season 6.";break;   
             case "ingsoc":case "INGSOC":case "Ingsoc": 
-                resultado = "INGSOC is the name of a die2nite meta created around season 4 by fward. More information: https://twinoid.com/g/ingsoc#";break;
+                resultado = "INGSOC is the name of a die2nite meta created around season 4 by fward. They did mostly Hardcore towns. INGSOC was disolved during MH Beta.";break;
             case "LS":case "ls":
-                resultado = "LS is an acronym to **Lost Souls**, a die2nite meta. LS was created on Season 4. More information: https://twinoid.com/g/lostsouls#";break;   
+                resultado = "LS is an acronym to **Lost Souls**, a die2nite meta. LS was created on Season 4.";break;   
             case "LMS":case "Lms":case "lms":
                 resultado = "LMS is an acronym to **L**ast **M**an **S**tanding, a distinction earned by the last player to die inside town. To be elegible to LMS, a player needs to be inside town during the last attack that will kill everyone inside town. LMS is random, so having more personal defense won't mather. Hardcore Towns award 2 LMS disctinctions: a regular LMS distinction and a Hardcore LMS distinction. Earning LMS would also earn some Hero days, but since May 2020 Hero days are now free. http://d2nwiki.com/wiki/Last_Man_Standing";break;
             case "MMJ": case "mmj":
@@ -130,49 +130,49 @@ client.on('message', message =>{
             case "MoS": case "mos": case "MOS":
                 resultado = "MoS is an acronym that stands for **M**inistry **o**f **S**lavery, a die2nite construction unlocked via Very Rare Blueprints (One of the preset VRs from A. Bunker e.ruins). MoS allows shunned players to use AP on constructions and the AP used this way contributes with a multiplier of 1.5 . https://github.com/Miudod2n/winstond2n/raw/master/images/mos.png Metas quite often make use of this construction by deliberating shunning some players that constitute the builder force of the town. This, altogether with scrapping (since shunned can't update external maps) and removing the shun later using Altar is a common strategy for when this blueprint is unlocked early http://d2nwiki.com/wiki/Ministry_of_Slavery.";break;
             case "N&C":case "n&c":
-                resultado = "N&C is an acronym to **Nick & Company**, a die2nite meta. N&C was created on Season 5 by VertigoProcess. More information: https://twinoid.com/g/n-c-meta#";break;   
+                resultado = "N&C is an acronym to **Nick & Company**, a die2nite meta. N&C was created on Season 5 by VertigoProcess.";break;   
             case "NHB":case "nhb":
                 resultado = "NHB stands for **N**o **H**oles **B**arred, a water construction that provides 150 rations. http://d2nwiki.com/wiki/No_Holes_Barred";break;
             case "OI":case "INN":case "inn": 
-                resultado = resto+" is one of the acronyms to **Oozing Inn**, a die2nite meta. OI was created on Season 1 by Bram. More information: https://twinoid.com/g/oozing-inn#";break;
+                resultado = resto+" is one of the acronyms to **Oozing Inn**, a die2nite meta. OI was created on Season 1 by Bram.";break;
             case "IBG":case "ibg":case "Ibg": 
                 resultado = resto+" is the acronym for **I**ndian **B**urial **G**round, a type of ruin. It spawns 3-6km from town. It is the only ruin that drops Meaty Bones. It has a negative 5% (1 upgrade) penalty for camping as topology. http://d2nwiki.com/wiki/Indian_Burial_Ground";break;
             case "P9C":case "p9c":
                 resultado = "P9C is an acronym to **Plan 9 Consortium**, a die2nite meta. P9C was proved to be a meta of multiple accounts and then disbanded.";break;
             case "RM":case "rm":
-                resultado = "RM is an acronym to **RandoMeta**, a die2nite meta. RM was created by ImbuedGreen on Season 7. Most of its members played as the Shadowclan or Clucky Bastards meta. More information: https://twinoid.com/g/randometa#";break;
+                resultado = "RM is an acronym to **RandoMeta**, a die2nite meta. RM was created by ImbuedGreen on Season 7. Most of its members played as the Shadowclan or Clucky Bastards meta.";break;
             case "RFM":case "rfm":
-                resultado = "RFM stands for **Royal Flying Mowers**, a die2nite meta. RFM was created on Season 3. More information: https://twinoid.com/g/rfm-royal-flying-mowers#";break;
+                resultado = "RFM stands for **Royal Flying Mowers**, a die2nite meta. RFM was created on Season 3.";break;
             case "RWF":case "rwf": 
                 resultado = "RWF stands for **Running With Fridges**, a die2nite meta. RWF was created by greveur on Season 4";break;             
             case "unnamed":case "Unnamed": 
-                resultado = "**Unnamed** was a die2nite meta. More information: https://twinoid.com/g/unnamed-meta#";break;
+                resultado = "**Unnamed** was a die2nite meta.";break;
             case "hive":case "Hive":
-                resultado = "**Hive** was a die2nite meta. More information: https://twinoid.com/g/hive#";break;
+                resultado = "**Hive** was a die2nite meta.";break;
             case "SC":case "sc": 
                 resultado = "SC stands for **Shadowclan**, a die2nite meta. Shadowclan was created on Season 5. It had other names like Clucky Bastards and RandoMeta";break; 
             case "SotA": 
-                resultado = "SotA stands for **Survivors of the Atom**, a die2nite meta who seeks Reactor distinctions. SotA was created on Season 13. More information: https://twinoid.com/g/sota-survivors-of-the-atom#";break;  
+                resultado = "SotA stands for **Survivors of the Atom**, a die2nite meta who seeks Reactor distinctions. SotA was created on Season 13.";break;  
             case "SS":case "ss": 
-                resultado = "SS stands for **Suicide Squad**, a die2nite meta. SS was created by pkeod on Season 12. More information: https://twinoid.com/g/suicide-squad#";break;
+                resultado = "SS stands for **Suicide Squad**, a die2nite meta. SS was created by pkeod on Season 12.";break;
             case "ST":case "st":
                 resultado = "ST stands for **S**earch **T**ower, a upgradable construction that reveal which direction was struck with replenishing winds. http://d2nwiki.com/wiki/Searchtower";break;
             case "SVV":case "svv": 
-                resultado = "SVV stands for **The Survivors**, a die2nite meta also known as Wandering Souls. SVV was created by Forgotten on Season 12. More information: https://twinoid.com/g/svv#";break;
+                resultado = "SVV stands for **The Survivors**, a die2nite meta also known as Wandering Souls. SVV was created by Forgotten on Season 12.";break;
             case "TZ":case "tz": 
-                resultado = "TZ stands for **Team Zero**, a die2nite meta. TZ was created by WalkingCrow on Season 6. More information: https://twinoid.com/g/team-zero#";break;   
+                resultado = "TZ stands for **Team Zero**, a die2nite meta. TZ was created by WalkingCrow on Season 6.";break;   
             case "TBS":case "tbs": 
                 resultado = "TBS stands for **The Bloodied Sleuths**, a die2nite meta. TBS was created on Season 6";break;
             case "UZC":case "uzc": 
-                resultado = "UZC stands for **Undead Zombie Chickens**, a die2nite meta. UZC was created by Zombieman on Season 11. More information: https://twinoid.com/g/the-chicken-army-aka-u-z-c-meta#";break;   
+                resultado = "UZC stands for **Undead Zombie Chickens**, a die2nite meta. UZC was created by Zombieman on Season 11.";break;   
             case "WCH":case "wch": 
-                resultado = "WCH stands for **Working Class Heroes**, a die2nite meta that had mostly Residents in its ranks. WCH was created on Season 5 by Miudo. More information: https://twinoid.com/g/working-class-heroes-meta#";break;   
+                resultado = "WCH stands for **Working Class Heroes**, a die2nite meta that had mostly Residents in its ranks. WCH was created on Season 5 by Miudo.";break;   
             case "WLB":case "wlb": 
                 resultado = "WLB is an acronym to **W**orn **L**eather **B**ag, a container item that holds Blueprints (BPs). http://d2nwiki.com/wiki/Worn_Leather_Bag";break;   
             case "WF":case "wf":case"wonfai":case"Wonfai":
                 resultado = resto+" stands for **Wonderful Failure**, a die2nite meta. WF was created by Sense on Season 1. More information: https://twinoid.com/g/wonderful-failure#";break;
             case "ZOA":case "zoa": 
-                resultado = "ZOA stands for **Zombies Outside Alliance**, a die2nite meta. ZOA was created by Embacie (in memoriam) on Season 6. More information: https://twinoid.com/g/zoa#";break;   
+                resultado = "ZOA stands for **Zombies Outside Alliance**, a die2nite meta. ZOA was created by Embacie (in memoriam) on Season 6.";break;   
             case "zoo":case "zombie zoo": case "Zoo":
                 resultado = "A zoo or **Zombie Zoo** is a game practice that involves clearing the zombies from the map except from one corner and maintaining them there. The benefits of a zombie zoo are the freedom of movement in the World Beyond and a better control of the map. To make a zombie zoo it is required a few days of scouting and camping and killing zombies from everywhere there shouldn't be zombies, in other words, from the zombie zoo corner. After the zoo is built some killing maintenance is necessary every other day to contain the spread. This is often called \"trim the zoo\". A zoo can be made on any corner the town so desires. It also depends on the placement of ruins and zombie patchs, as well as distance and resources available (both human and items). Sometimes towns that can't make a corner zoo try a \"half zoo\" which is a zoo occupying half of the map.\n Creating a zombie zoo might cause a global zombie respawn if the amount of zombies killed in its making surpass a certain threshold that depends on the map size.";break;   
 //**
